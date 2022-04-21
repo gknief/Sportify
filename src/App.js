37b1a38c812d53a64144d735eb52379281bfd4fb
Grant Knief <gknief@mac.com>
2022-04-21 18:19:04 -0400

    got access token to save to local storage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
@@ -6,15 +7,31 @@ function App() {
   const REDIRECT_URI = "http://localhost:3000";
   const AUTH_ENDPOINT = "http://accounts.spotify.com/authorize";
   const RESPONSE_TYPE = "token";
+  const [token, setToken] = useState("");
+  
+  
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    const hash = window.location.hash;
+    if(!token && hash) {
+      const accessToken = hash.split("&")[0].split("=")[1];
+      window.localStorage.setItem("token", accessToken);
+      setToken(accessToken);
+    } else {
+      setToken(token);
+    }
+  }, []) 
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Authenticate with Spotify</a>
       </header>
-      <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Authenticate with Spotify</a>
     </div>
   );
 }
 
 export default App;
+
+
+// "BQCA8HJCpb0Gar2deEESLzJbz8bFwctTrMuhENQBX-bXAIHcaWtkF8WTaTXp6pS3utWiuurDEbtFwwVrHu0PTnVUWPstg6v3d7aY_iL5ddCZRF9DPjdxT_nub0sFUo6ylCFXuYDbAKcEUkv7jHcxkVKzVxewy3KT3lE"
