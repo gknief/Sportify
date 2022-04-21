407f8a0ed3340782128bd979191f0451a1830a01
Grant Knief <gknief@mac.com>
2022-04-21 17:37:50 -0400

    integrated spotify authentication

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import './App.css';
 
 function App() {
   const CLIENT_ID = "8defe2c2d84640299260e08492c3d840";
-  const REDIRECT_URL = "http://localhost:3000";
-  const AUTH_ENDPOINT = "https://accounts.spotify.authorize";
+  const REDIRECT_URI = "http://localhost:3000";
+  const AUTH_ENDPOINT = "http://accounts.spotify.com/authorize";
   const RESPONSE_TYPE = "token";
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
       </header>
+      <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Authenticate with Spotify</a>
     </div>
   );
 }
